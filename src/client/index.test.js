6d469e48511d7248780be8eece9047fd0b65dbf4
Alex Sanders <alex@sndrs.dev>
2024-08-22 12:48:47 +0100

    tests

---
+++ src/client/index.test.js
@@ -0,0 +1,7 @@
+import assert from 'node:assert';
+import test from 'node:test';
+
+// just a dummy test to make sure the test runner is working
+test('synchronous passing test', () => {
+	assert.strictEqual(1, 1);
+});
